5bd8b39e65287aa24a488a6c74812df53843e944
Soldier246 <dcasallaszoriano@gmail.com>
2023-04-19 17:27:09 -0500

    probando modificaciones

--- js/catalogo.js
+++ js/catalogo.js
@@ -3,16 +3,11 @@ let star2 = document.getElementById("star2");
 let star3 = document.getElementById("star3");
 let star4 = document.getElementById("star4");
 let star5 = document.getElementById("star5");
-
-
-
 let star6 = document.getElementById("star6");
 let star7 = document.getElementById("star7");
 let star8 = document.getElementById("star8");
 let star9 = document.getElementById("star9");
 let star10 = document.getElementById("star10");
-
-
 let star11 = document.getElementById("star11");
 let star12 = document.getElementById("star12");
 let star13 = document.getElementById("star13");
@@ -21,194 +16,47 @@ let star15 = document.getElementById("star15");
 
 
 star1.addEventListener("click", function () {
-    star1.classList.remove("fa-regular");
-    star1.classList.add("fa-solid");
-    star2.classList.remove("fa-solid");
-    star2.classList.add("fa-regular");
-    star3.classList.remove("fa-solid");
-    star3.classList.add("fa-regular");
-    star4.classList.remove("fa-solid");
-    star4.classList.add("fa-regular");
-    star5.classList.remove("fa-solid");
-    star5.classList.add("fa-regular");
+    starUno(star1, star2, star3, star4, star5)
 });
 star2.addEventListener("click", function () {
-    star1.classList.remove("fa-regular");
-    star1.classList.add("fa-solid");
-    star2.classList.remove("fa-regular");
-    star2.classList.add("fa-solid");
-    star3.classList.remove("fa-solid");
-    star3.classList.add("fa-regular");
-    star4.classList.remove("fa-solid");
-    star4.classList.add("fa-regular");
-    star5.classList.remove("fa-solid");
-    star5.classList.add("fa-regular");
+    starDos(star1, star2, star3, star4, star5)
 });
 star3.addEventListener("click", function () {
-    star1.classList.remove("fa-regular");
-    star1.classList.add("fa-solid");
-    star2.classList.remove("fa-regular");
-    star2.classList.add("fa-solid");
-    star3.classList.remove("fa-regular");
-    star3.classList.add("fa-solid");
-    star4.classList.remove("fa-solid");
-    star4.classList.add("fa-regular");
-    star5.classList.remove("fa-solid");
-    star5.classList.add("fa-regular");
+    starTres(star1, star2, star3, star4, star5)
 });
 star4.addEventListener("click", function () {
-    star1.classList.remove("fa-regular");
-    star1.classList.add("fa-solid");
-    star2.classList.remove("fa-regular");
-    star2.classList.add("fa-solid");
-    star3.classList.remove("fa-regular");
-    star3.classList.add("fa-solid");
-    star4.classList.remove("fa-regular");
-    star4.classList.add("fa-solid");
-    star5.classList.remove("fa-solid");
-    star5.classList.add("fa-regular");
+    starCuatro(star1, star2, star3, star4, star5)
 });
 star5.addEventListener("click", function () {
-    star1.classList.remove("fa-regular");
-    star1.classList.add("fa-solid");
-    star2.classList.remove("fa-regular");
-    star2.classList.add("fa-solid");
-    star3.classList.remove("fa-regular");
-    star3.classList.add("fa-solid");
-    star4.classList.remove("fa-regular");
-    star4.classList.add("fa-solid");
-    star5.classList.remove("fa-regular");
-    star5.classList.add("fa-solid");
+    starCinco(star1, star2, star3, star4, star5)
 });
-
-
-
-
-
-
 star6.addEventListener("click", function () {
-    star6.classList.remove("fa-regular");
-    star6.classList.add("fa-solid");
-    star7.classList.remove("fa-solid");
-    star7.classList.add("fa-regular");
-    star8.classList.remove("fa-solid");
-    star8.classList.add("fa-regular");
-    star9.classList.remove("fa-solid");
-    star9.classList.add("fa-regular");
-    star10.classList.remove("fa-solid");
-    star10.classList.add("fa-regular");
+    starUno(star6, star7, star8, star9, star10)
 });
 star7.addEventListener("click", function () {
-    star6.classList.remove("fa-regular");
-    star6.classList.add("fa-solid");
-    star7.classList.remove("fa-regular");
-    star7.classList.add("fa-solid");
-    star8.classList.remove("fa-solid");
-    star8.classList.add("fa-regular");
-    star9.classList.remove("fa-solid");
-    star9.classList.add("fa-regular");
-    star10.classList.remove("fa-solid");
-    star10.classList.add("fa-regular");
+    starDos(star6, star7, star8, star9, star10)
 });
 star8.addEventListener("click", function () {
-    star6.classList.remove("fa-regular");
-    star6.classList.add("fa-solid");
-    star7.classList.remove("fa-regular");
-    star7.classList.add("fa-solid");
-    star8.classList.remove("fa-regular");
-    star8.classList.add("fa-solid");
-    star9.classList.remove("fa-solid");
-    star9.classList.add("fa-regular");
-    star10.classList.remove("fa-solid");
-    star10.classList.add("fa-regular");
+    starTres(star6, star7, star8, star9, star10)
 });
 star9.addEventListener("click", function () {
-    star6.classList.remove("fa-regular");
-    star6.classList.add("fa-solid");
-    star7.classList.remove("fa-regular");
-    star7.classList.add("fa-solid");
-    star8.classList.remove("fa-regular");
-    star8.classList.add("fa-solid");
-    star9.classList.remove("fa-regular");
-    star9.classList.add("fa-solid");
-    star10.classList.remove("fa-solid");
-    star10.classList.add("fa-regular");
+    starCuatro(star6, star7, star8, star9, star10)
 });
 star10.addEventListener("click", function () {
-    star6.classList.remove("fa-regular");
-    star6.classList.add("fa-solid");
-    star7.classList.remove("fa-regular");
-    star7.classList.add("fa-solid");
-    star8.classList.remove("fa-regular");
-    star8.classList.add("fa-solid");
-    star9.classList.remove("fa-regular");
-    star9.classList.add("fa-solid");
-    star10.classList.remove("fa-regular");
-    star10.classList.add("fa-solid");
+    starCinco(star6, star7, star8, star9, star10)
 });
-
-
-
-
-
-
 star11.addEventListener("click", function () {
-    star11.classList.remove("fa-regular");
-    star11.classList.add("fa-solid");
-    star12.classList.remove("fa-solid");
-    star12.classList.add("fa-regular");
-    star13.classList.remove("fa-solid");
-    star13.classList.add("fa-regular");
-    star14.classList.remove("fa-solid");
-    star14.classList.add("fa-regular");
-    star15.classList.remove("fa-solid");
-    star15.classList.add("fa-regular");
+    starUno(star11, star12, star13, star14, star15)
 });
 star12.addEventListener("click", function () {
-    star11.classList.remove("fa-regular");
-    star11.classList.add("fa-solid");
-    star12.classList.remove("fa-regular");
-    star12.classList.add("fa-solid");
-    star13.classList.remove("fa-solid");
-    star13.classList.add("fa-regular");
-    star14.classList.remove("fa-solid");
-    star14.classList.add("fa-regular");
-    star15.classList.remove("fa-solid");
-    star15.classList.add("fa-regular");
+    starDos(star11, star12, star13, star14, star15)
 });
 star13.addEventListener("click", function () {
-    star11.classList.remove("fa-regular");
-    star11.classList.add("fa-solid");
-    star12.classList.remove("fa-regular");
-    star12.classList.add("fa-solid");
-    star13.classList.remove("fa-regular");
-    star13.classList.add("fa-solid");
-    star14.classList.remove("fa-solid");
-    star14.classList.add("fa-regular");
-    star15.classList.remove("fa-solid");
-    star15.classList.add("fa-regular");
+    starTres(star11, star12, star13, star14, star15)
 });
 star14.addEventListener("click", function () {
-    star11.classList.remove("fa-regular");
-    star11.classList.add("fa-solid");
-    star12.classList.remove("fa-regular");
-    star12.classList.add("fa-solid");
-    star13.classList.remove("fa-regular");
-    star13.classList.add("fa-solid");
-    star14.classList.remove("fa-regular");
-    star14.classList.add("fa-solid");
-    star15.classList.remove("fa-solid");
-    star15.classList.add("fa-regular");
+    starCuatro(star11, star12, star13, star14, star15)
 });
 star15.addEventListener("click", function () {
-    star11.classList.remove("fa-regular");
-    star11.classList.add("fa-solid");
-    star12.classList.remove("fa-regular");
-    star12.classList.add("fa-solid");
-    star13.classList.remove("fa-regular");
-    star13.classList.add("fa-solid");
-    star14.classList.remove("fa-regular");
-    star14.classList.add("fa-solid");
-    star15.classList.remove("fa-regular");
-    star15.classList.add("fa-solid");
+    starCinco(star11, star12, star13, star14, star15)
 });
---
+++ js/funciones.js
@@ -0,0 +1,61 @@
+
+function starUno(star1, star2, star3, star4, star5) {
+    star1.classList.remove("fa-regular");
+    star1.classList.add("fa-solid");
+    star2.classList.remove("fa-solid");
+    star2.classList.add("fa-regular");
+    star3.classList.remove("fa-solid");
+    star3.classList.add("fa-regular");
+    star4.classList.remove("fa-solid");
+    star4.classList.add("fa-regular");
+    star5.classList.remove("fa-solid");
+    star5.classList.add("fa-regular");
+}
+function starDos(star1, star2, star3, star4, star5) {
+    star1.classList.remove("fa-regular");
+    star1.classList.add("fa-solid");
+    star2.classList.remove("fa-regular");
+    star2.classList.add("fa-solid");
+    star3.classList.remove("fa-solid");
+    star3.classList.add("fa-regular");
+    star4.classList.remove("fa-solid");
+    star4.classList.add("fa-regular");
+    star5.classList.remove("fa-solid");
+    star5.classList.add("fa-regular");
+}
+function starTres(star1, star2, star3, star4, star5) {
+    star1.classList.remove("fa-regular");
+    star1.classList.add("fa-solid");
+    star2.classList.remove("fa-regular");
+    star2.classList.add("fa-solid");
+    star3.classList.remove("fa-regular");
+    star3.classList.add("fa-solid");
+    star4.classList.remove("fa-solid");
+    star4.classList.add("fa-regular");
+    star5.classList.remove("fa-solid");
+    star5.classList.add("fa-regular");
+}
+function starCuatro(star1, star2, star3, star4, star5) {
+    star1.classList.remove("fa-regular");
+    star1.classList.add("fa-solid");
+    star2.classList.remove("fa-regular");
+    star2.classList.add("fa-solid");
+    star3.classList.remove("fa-regular");
+    star3.classList.add("fa-solid");
+    star4.classList.remove("fa-regular");
+    star4.classList.add("fa-solid");
+    star5.classList.remove("fa-solid");
+    star5.classList.add("fa-regular");
+}
+function starCinco(star1, star2, star3, star4, star5) {
+    star1.classList.remove("fa-regular");
+    star1.classList.add("fa-solid");
+    star2.classList.remove("fa-regular");
+    star2.classList.add("fa-solid");
+    star3.classList.remove("fa-regular");
+    star3.classList.add("fa-solid");
+    star4.classList.remove("fa-regular");
+    star4.classList.add("fa-solid");
+    star5.classList.remove("fa-regular");
+    star5.classList.add("fa-solid");
+}

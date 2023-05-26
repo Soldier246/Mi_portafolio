732418580e4a557e00ed83d5661dfb9498f7af90
LobitoM365 <almariojuancho@gmail.com>
2023-05-25 21:21:13 -0500

    probando modificaciones

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let menuBar = document.getElementById("menuBar");
+let menuDesplegable = document.getElementById("menuDesplegable");
+let padreSubmenu = document.getElementById("padreSubmenu");
+let submenu = document.getElementById("submenu");
+let i = document.getElementById("i");
+
+menuBar.addEventListener("click", function () {
+    if (menuDesplegable.classList.contains("ul-transition-replegar")) {
+        menuDesplegable.classList.remove("ul-transition-replegar");
+        menuDesplegable.classList.add("ul-transition-desplegar")
+    } else {
+        menuDesplegable.classList.add("ul-transition-replegar")
+        menuDesplegable.classList.remove("ul-transition-desplegar")
+        submenu.classList.remove("submenu-transition");
+        submenu.classList.add("submenu-invisible");
+        i.classList.remove("i-transition");
+    }
+})
+padreSubmenu.addEventListener("click", function () {
+    if (submenu.classList.contains("submenu-transition")) {
+        submenu.classList.remove("submenu-transition");
+        submenu.classList.add("submenu-invisible");
+        i.classList.remove("i-transition");
+    } else {
+        submenu.classList.add("submenu-transition")
+        submenu.classList.remove("submenu-invisible");
+        i.classList.add("i-transition");
+    }
+})

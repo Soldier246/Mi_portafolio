c615c3f86c0d3452c0b72912bad265450a4fffdb
Soldier246 <dcasallaszoriano@gmail.com>
2023-10-03 21:39:57 -0500

    pandora2.0

--- js/pandora20.js
+++ js/pandora20.js
@@ -1,12 +1,14 @@
 let imgSlider = document.querySelectorAll(".div-img-slider");
 let chevronRight = document.getElementById("chevronRight");
+let chevronLeft = document.getElementById("chevronLeft");
 
-let margin = -100;
+let margin = -200;
 let data = [];
 for (let x = 0; x < imgSlider.length; x++) {
     margin = margin + 100;
     data[x] = margin;
     imgSlider[x].style.marginLeft = margin + "%";
+    console.log(imgSlider.length)
 }
 
 let intervalId;
@@ -19,10 +21,19 @@ chevronRight.addEventListener("click", function () {
     clearInterval(intervalId);
     interval()
 })
+chevronLeft.addEventListener("click", function () {
 
+    for (let x = 0; x < imgSlider.length; x++) {
+        data[x] = data[x] + 100;
+    }
+    console.log(data)
+    volver(data);
+    clearInterval(intervalId);
+    interval()
+})
 function ir(data) {
     for (let x = 0; x < data.length; x++) {
-        if (data[x] == -200) {
+        if (data[x] == (imgSlider.length - 1) * -100) {
             data[x] = 100;
             imgSlider[x].style.transition = "margin 0s";
             console.log(data[x])
@@ -34,12 +45,29 @@ function ir(data) {
         }
     }
 }
+function volver(data) {
+    
+    for (let x = 0; x < data.length; x++) {
+        if (data[x] == (imgSlider.length - 1 ) * 100) {
+            console.log(data)
+            data[x] = -100;
+            imgSlider[x].style.transition = "margin 0s";
+            console.log(data[x])
+            imgSlider[x].style.marginLeft = -100 + "%";
+        } else {
+            imgSlider[x].style.transition = "margin 0.5s";
+            imgSlider[x].style.marginLeft = data[x] + "%";
+
+        }
+    }
+}
 function interval() {
     intervalId = setInterval(() => {
         for (let x = 0; x < imgSlider.length; x++) {
             data[x] = data[x] - 100;
+            console.log(data)
         }
         ir(data);
-    }, 2000);
+    }, 5000);
 }
 interval();
